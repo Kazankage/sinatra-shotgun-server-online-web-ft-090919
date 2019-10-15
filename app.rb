0aec623478548f2c7117eb22830333a830648594
Randel Jiao <randeljiao@gmail.com>
2019-10-15 22:53:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
+  get '/chicken' do 
     "Smile. Tho your heart is aching! Life is worth while. You can do it buddy! Test this"
   end
 
